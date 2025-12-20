b33dbb2091a0670967d0cea565c5a5e3abdf3be4
Akito <akito1224m@gmail.com>
2025-12-20 16:32:53 +0900

    デバッグ用: displayErrorDetailsを一時的にtrueに設定

--- app/settings.php
+++ app/settings.php
@@ -13,7 +13,7 @@ return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         SettingsInterface::class => function () {
             return new Settings([
-                'displayErrorDetails' => false, // Should be set to false in production
+                'displayErrorDetails' => true, // 一時的にtrueでエラー詳細を確認
                 'logError'            => true,
                 'logErrorDetails'     => true,
                 'logger' => [
